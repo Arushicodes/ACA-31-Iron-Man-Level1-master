d3b497c993c6a68bc8f8da891382b11f4722fcb7
Arushicodes <adityaaarushi43196@gmail.com>
2021-10-06 18:47:47 +0530

    ACA 32

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 var iron, ironImg;
 var bg, backgroundImg;
+var stoneGroup, stoneImg;
 
 function preload() {
   backgroundImg = loadImage("images/bg.jpg");
   ironImg= loadImage("images/iron.png");
+  stoneImg=loadImage("images/stone.png")
 
  
  
@@ -13,18 +15,26 @@ function setup() {
   createCanvas(1000, 600);
   bg = createSprite(580,300);
   bg.addImage(backgroundImg);
+  bg.scale=2
+  bg.velocityY=-5
   iron = createSprite(200,505,20,50);
   iron.addImage(ironImg);
   iron.scale= 0.5
   ground = createSprite(200,585,400,10);
  
   ground.visible = false;
+  stoneGroup=new Group()
 }
 
 function draw() {
   if(keyDown("space") ) {
     iron.velocityY = -16;
   }
+  if(iron.y<50){
+    iron.y=50;
+  }
+  if(bg.y<200)
+  bg.y=bg.width/4
 
   iron.velocityY = iron.velocityY + 0.5;
   if(keyDown("left") )
@@ -32,9 +42,29 @@ function draw() {
   if(keyDown("right") )
   iron.x +=5
   iron.collide(ground);
- 
+ generateStones();
+ for(var i = 0 ; i< (stoneGroup).length ;i++){
+  var temp = (stoneGroup).get(i) ;
+  
+  if (temp.isTouching(iron)) {
+     iron.collide(temp);
+    }
+      
+  }
+
     
     drawSprites();
    
 }
-
+function generateStones(){
+  if (frameCount % 70 === 0) {
+    var stone = createSprite(1200,120,40,10);
+    stone.x = random(50,450);
+    stone.addImage(stoneImg);
+    stone.scale = 0.5;
+    stone.velocityY = 5;
+    
+    stone.lifetime =250;
+    stoneGroup.add(stone);
+  }
+}
